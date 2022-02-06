04e0d4f2f3ee8ea1fa5982270d5d48c8832f8abe
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-02-06 19:30:47 +0100

    added the question design

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,3 +1,17 @@
 import React from 'react';
 
-export default function Question() {}
+export default function Question() {
+  return (
+    <div className="question-container">
+      <div className="question">
+        <h1>What is a Question?</h1>
+      </div>
+      <div className="options">
+        <button>Option 1</button>
+        <button>Option 2</button>
+        <button>Option 3</button>
+        <button>Option 4</button>
+      </div>
+    </div>
+  );
+}
