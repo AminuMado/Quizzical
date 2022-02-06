3d7c0353169947e483fea1a2a04c8b2f8d47683b
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-02-06 19:28:25 +0100

    add the start page design

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,14 +1,17 @@
 import React from 'react';
-
+import blob1_Src from '../../public/blobs.png';
+import blob2_Src from '../../public/blob5.png';
 export default function Start() {
   return (
-    <div>
+    <div className="start-container">
       <h1>Quizzical</h1>
       <p>
         This is quiz based game.Try it out, Its fun.Lets see how much
         trivia you have in you
       </p>
-      <button> Start Game</button>
+      <button> Start Quiz</button>
+      <img src={blob1_Src} className="start-blob1"></img>
+      <img src={blob2_Src} className="start-blob2"></img>
     </div>
   );
 }
