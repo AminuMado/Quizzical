bbaa74bfe8b9d631727ea5076a5484af12fee29f
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-02-06 19:27:52 +0100

    added the main page design

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import blob1_Src from '../../public/blobs.png';
+import blob2_Src from '../../public/blob5.png';
 
 export default function Main() {
-  return <h1>This is the Main Component</h1>;
+  return (
+    <div className="main-container">
+      <button> Check Answers</button>
+      <img src={blob1_Src} className="main-blob1"></img>
+      <img src={blob2_Src} className="main-blob2"></img>
+    </div>
+  );
 }
